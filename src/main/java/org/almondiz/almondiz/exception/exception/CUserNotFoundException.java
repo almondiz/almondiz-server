3dbcf1e500a4b4ac253be50700f9dbd500d8563c
yejipractice <twoye1193@ajou.ac.kr>
2022-08-16 22:41:02 +0900

    [feature] #7 Exception ControllerAdvice 생성 및 UserNotFound 예외 custom 생성

---
+++ src/main/java/org/almondiz/almondiz/exception/exception/CUserNotFoundException.java
@@ -0,0 +1,17 @@
+package org.almondiz.almondiz.exception.exception;
+
+public class CUserNotFoundException extends RuntimeException {
+
+    public CUserNotFoundException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    public CUserNotFoundException(String msg) {
+        super(msg);
+    }
+
+    public CUserNotFoundException() {
+        super();
+    }
+}
+
