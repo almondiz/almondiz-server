16df6e6b4f98531e4770a8ecca670dd6ef42e91c
yejipractice <twoye1193@ajou.ac.kr>
2022-08-28 16:49:37 +0900

    [feature] #16 Notification 엔티티 및 repo 생성

---
+++ src/main/java/org/almondiz/almondiz/notification/NotificationType.java
@@ -0,0 +1,13 @@
+package org.almondiz.almondiz.notification;
+
+public enum NotificationType {
+    // 임시 메세지
+    ALERT("관련 알림이 도착했습니다"),
+    FOLLOW("님이 팔로우하셨습니다");
+
+    private String message;
+
+    NotificationType(String message) {
+        this.message = message;
+    }
+}
