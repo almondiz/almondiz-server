4b9f9371d055ece3e386c2889706c1861494bfb6
yejipractice <twoye1193@ajou.ac.kr>
2022-09-01 15:43:55 +0900

    [feature] #15 Follow 엔티티 및 repo 생성

---
+++ src/main/java/org/almondiz/almondiz/follow/FollowRepository.java
@@ -0,0 +1,14 @@
+package org.almondiz.almondiz.follow;
+
+import org.almondiz.almondiz.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    Optional<Follow> findByFollowerAndFollowee(User follower, User followee);
+    Optional<Follow> findById(Long id);
+    List<Follow> findAllByFollowee(User followee);
+    List<Follow> findAllByFollower(User follower);
+}
