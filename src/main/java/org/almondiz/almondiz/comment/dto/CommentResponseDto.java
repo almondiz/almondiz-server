e4b5db2a89aefa684ff2e78280e05ea094215dfe
coderclcl <gattenmaster@gmail.com>
2022-09-07 20:25:12 +0900

    [feature] #18 CommentResponseDto, PostResponseDto에 createdAt 추가

--- src/main/java/org/almondiz/almondiz/comment/dto/CommentResponseDto.java
+++ src/main/java/org/almondiz/almondiz/comment/dto/CommentResponseDto.java
@@ -1,5 +1,6 @@
 package org.almondiz.almondiz.comment.dto;
 
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -20,11 +21,14 @@ public class CommentResponseDto {
 
     private String text;
 
+    private LocalDateTime createdAt;
+
     public CommentResponseDto(Comment comment, String nickName) {
         this.commentId = comment.getCommentId();
         this.userId = comment.getUser().getUserId();
         this.nickName = nickName;
         this.text = comment.getText();
+        this.createdAt = comment.getCreatedAt();
     }
 
 }
