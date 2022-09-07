662cac844697090655aaca52f5e60ce3bbd3fa99
coderclcl <gattenmaster@gmail.com>
2022-09-07 20:07:23 +0900

    [feature] #19 피드디테일 페이지 tagList 추가

--- src/main/java/org/almondiz/almondiz/tagpost/TagPostService.java
+++ src/main/java/org/almondiz/almondiz/tagpost/TagPostService.java
@@ -1,2 +1,32 @@
-package org.almondiz.almondiz.tagpost;public class TagPostService {
+package org.almondiz.almondiz.tagpost;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.almondiz.almondiz.post.entity.Post;
+import org.almondiz.almondiz.tag.dto.TagResponseDto;
+import org.almondiz.almondiz.tagpost.entity.TagPost;
+import org.almondiz.almondiz.tagpost.entity.TagPostRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TagPostService {
+
+    private final TagPostRepository tagPostRepository;
+
+    @Transactional
+    public List<TagResponseDto> getTagsByPost(Post post){
+        return tagPostRepository.findByPost(post)
+            .stream().map(tagPost -> this.getTagResponseDto(tagPost))
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public TagResponseDto getTagResponseDto(TagPost tagPost){
+        return new TagResponseDto(tagPost.getTag());
+    }
+
+
 }
