94c5cac97575935ec46f5dc16709f563c00e6f40
yejipractice <twoye1193@ajou.ac.kr>
2022-09-20 01:26:05 +0900

    [bugfix] #45 cors error

--- src/main/java/org/almondiz/almondiz/config/security/SecurityConfig.java
+++ src/main/java/org/almondiz/almondiz/config/security/SecurityConfig.java
@@ -4,11 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.almondiz.almondiz.user.AuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -32,37 +30,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .formLogin().disable()
+            .httpBasic().disable()
             .csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .formLogin().disable()
-            .httpBasic().disable()
-            .apply(new customDsl())
-            .and()
             .authorizeRequests()
-            .antMatchers("/api/user/**").access("hasRole('ROLE_USER')")
+            .antMatchers("/api/user/**")
+            .access("hasRole('ROLE_USER')")
             .anyRequest().permitAll()
             .and()
             .exceptionHandling().accessDeniedHandler(new CAccessDeniedHandler())
             .and()
             .exceptionHandling().authenticationEntryPoint(new CAuthenticationEntryPoint())
             .and()
+            .addFilter(corsConfig.corsFilter())
             .addFilterBefore(new JwtAuthenticationFilter(authService), UsernamePasswordAuthenticationFilter.class);
         http.headers().frameOptions().disable();
         return http.build();
-    }
-
-
-
-    public class customDsl extends AbstractHttpConfigurer<customDsl, HttpSecurity> {
-
-        @Override
-        public void configure(HttpSecurity http) {
-            AuthenticationManager authenticationManager = http.getSharedObject(
-                AuthenticationManager.class);
-            http
-                .addFilter(corsConfig.corsFilter());
-        }
-
     }
 }
